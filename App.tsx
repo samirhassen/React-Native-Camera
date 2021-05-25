239367ceab9d2661cd7186a11f338600c54b11c4
samirhassen <hassen.samir@gmail.com>
2021-05-25 14:32:54 +0200

    'Initial'

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { StrictMode, useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+
+camera: Camera;
+
+function snapPic(){
+  (async () => {
+    })();
+}
 
 export default function App() {
+const [hasPermission, setPermission] = useState(false);
+const [selectedCamera, setSelectedCamera] = useState(Camera.Constants.Type.back);
+
+useEffect(() => {
+  (async () => { 
+    const { granted } = await Camera.requestPermissionsAsync()
+    setPermission(granted);
+  })();
+}, [])
+
+if(!hasPermission) {
+  return <View><Text>If we cant use your camera u cant use us</Text></View>;
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Camera style={styles.camera} type={selectedCamera}>
+        <View style={styles.textContainer}>
+          <TouchableOpacity onPress={snapPic}>
+            <Text style={styles.snapText}>Snap Pic</Text>
+          </TouchableOpacity>
+          </View> 
+      </Camera>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +43,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  camera: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  textContainer: {
+    flex: 1,
+    backgroundColor: '#000',
+    height: 60,
+    alignSelf: 'flex-end',
     justifyContent: 'center',
   },
+  snapText: {
+    color: '#fff',
+    fontSize: 14,
+    alignSelf: 'center',
+  }
 });
